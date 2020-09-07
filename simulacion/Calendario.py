5db3e87c60f01b50eaa24e44c7e7f394a2ff41a9
Agustin Laborde <alaborde@uc.cl>
2020-09-06 23:29:16 -0300

    empezando el calendario

---
+++ simulacion/Calendario.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+calendario = {}
+semana={"lunes":"0","martes":"1","miercoles":"2","jueves":"3","viernes":"4","sabado":"5","domingo":"6"}
+modulo={""}
+for i in range (0,2):
+    calendario[i]=semana
+print (calendario)
+
+
+# In[ ]:
+
+
+
+
