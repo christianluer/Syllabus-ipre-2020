3ff45cc20ada2e39ed67966020de79d15aee62f5
Agustin Laborde <alaborde@uc.cl>
2020-09-07 00:18:50 -0300

    avanzando un poco el calendario

--- simulacion/Calendario.py
+++ simulacion/Calendario.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[14]:
 
 
 calendario = {}
 semana={"lunes":"0","martes":"1","miercoles":"2","jueves":"3","viernes":"4","sabado":"5","domingo":"6"}
-modulo={""}
+modulo={}
+for i in range (1,41):
+    modulo[i]=i
+semana={"lunes":modulo,"martes":modulo,"miercoles":modulo,"jueves":modulo,"viernes":modulo,"sabado":modulo,"domingo":modulo}
 for i in range (0,2):
     calendario[i]=semana
 print (calendario)
