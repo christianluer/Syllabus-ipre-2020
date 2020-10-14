27805b43c92275419a0ced6b00118f4afb2ea8f3
Barbara Calderon <barbaracalrom@uc.cl>
2020-10-14 11:46:52 -0300

    corriendo pero infactible

--- gurobi/main.py
+++ gurobi/main.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # DÍAS
 
-T = [str(k) for k in range(1,13)] 
+T = [int(k) for k in range(1,13)] 
 # 14 dias - 1, 2, ..., 13
 
 
@@ -53,79 +53,79 @@ for p in P:
 
 #Valores iniciales
 
-w_pst_dada[1,1,1]=3
-
-w_pst_dada[2,1,1]=1
-
-w_pst_dada[3,1,1]=2
-
-w_pst_dada[2,1,2]=1
-
-w_pst_dada[1,2,2]=3
-
-w_pst_dada[2,2,2]=1
-
-w_pst_dada[1,1,3]=1
-
-w_pst_dada[1,3,3]=3
-
-w_pst_dada[3,2,3]=2
-
-w_pst_dada[2,2,3]=1
-
-w_pst_dada[1,2,4]=1
-
-w_pst_dada[1,1,5]=1
-
-w_pst_dada[1,3,5]=1
-
-w_pst_dada[2,1,5]=1
-
-w_pst_dada[3,3,5]=2
-
-w_pst_dada[1,2,6]=1
-
-w_pst_dada[1,4,6]=3
-
-w_pst_dada[2,2,6]=1
-
-w_pst_dada[1,3,7]=1
-
-w_pst_dada[1,5,7]=3
-
-w_pst_dada[2,1,7]=1
-
-w_pst_dada[3,4,7]=2
-
-w_pst_dada[1,4,8]=1
-
-w_pst_dada[1,6,8]=3
-
-w_pst_dada[2,2,8]=1
-
-w_pst_dada[1,5,9]=1
-
-w_pst_dada[2,3,9]=1
-
-w_pst_dada[3,5,9]=2
-
-w_pst_dada[1,4,10]=1
-
-w_pst_dada[1,6,10]=1
-
-w_pst_dada[2,3,10]=1
-
-w_pst_dada[2,4,10]=1
-
-w_pst_dada[1,5,11]=1
-
-w_pst_dada[2,4,11]=1
-
-w_pst_dada[3,6,11]=2
-
-w_pst_dada[1,6,12]=1
-
-w_pst_dada[3,1,12]=2
+#w_pst_dada[1,1,1]=3
+#
+#w_pst_dada[2,1,1]=1
+#
+#w_pst_dada[3,1,1]=2
+#
+#w_pst_dada[2,1,2]=1
+#
+#w_pst_dada[1,2,2]=3
+#
+#w_pst_dada[2,2,2]=1
+#
+#w_pst_dada[1,1,3]=1
+#
+#w_pst_dada[1,3,3]=3
+#
+#w_pst_dada[3,2,3]=2
+#
+#w_pst_dada[2,2,3]=1
+#
+#w_pst_dada[1,2,4]=1
+#
+#w_pst_dada[1,1,5]=1
+#
+#w_pst_dada[1,3,5]=1
+#
+#w_pst_dada[2,1,5]=1
+#
+#w_pst_dada[3,3,5]=2
+#
+#w_pst_dada[1,2,6]=1
+#
+#w_pst_dada[1,4,6]=3
+#
+#w_pst_dada[2,2,6]=1
+#
+#w_pst_dada[1,3,7]=1
+#
+#w_pst_dada[1,5,7]=3
+#
+#w_pst_dada[2,1,7]=1
+#
+#w_pst_dada[3,4,7]=2
+#
+#w_pst_dada[1,4,8]=1
+#
+#w_pst_dada[1,6,8]=3
+#
+#w_pst_dada[2,2,8]=1
+#
+#w_pst_dada[1,5,9]=1
+#
+#w_pst_dada[2,3,9]=1
+#
+#w_pst_dada[3,5,9]=2
+#
+#w_pst_dada[1,4,10]=1
+#
+#w_pst_dada[1,6,10]=1
+#
+#w_pst_dada[2,3,10]=1
+#
+#w_pst_dada[2,4,10]=1
+#
+#w_pst_dada[1,5,11]=1
+#
+#w_pst_dada[2,4,11]=1
+#
+#w_pst_dada[3,6,11]=2
+#
+#w_pst_dada[1,6,12]=1
+#
+#w_pst_dada[3,1,12]=2
 
 
 # Variable de estado R-p
@@ -159,11 +159,13 @@ for p in P:
 
 # Valores Iniciales
 
+
 z_p_dada[1] = 5
 
 z_p_dada[2] = 3
 
 z_p_dada[3] = 4
+
 
 
 # Variable de estado X-p,t
@@ -180,23 +182,23 @@ for p in P:
 
 # Valores Iniciales
 
-x_pt_dada[1,1] = 3
-
-x_pt_dada[2,1] = 1
-
-x_pt_dada[3,1] = 2
-
-x_pt_dada[2,2] = 1
-
-x_pt_dada[1,3] = 1
-
-x_pt_dada[1,5] = 1
-
-x_pt_dada[2,5] = 1
-
-x_pt_dada[2,7] = 1
-
-x_pt_dada[3,12] = 2
+#x_pt_dada[1,1] = 3
+#
+#x_pt_dada[2,1] = 1
+#
+#x_pt_dada[3,1] = 2
+#
+#x_pt_dada[2,2] = 1
+#
+#x_pt_dada[1,3] = 1
+#
+#x_pt_dada[1,5] = 1
+#
+#x_pt_dada[2,5] = 1
+#
+#x_pt_dada[2,7] = 1
+#
+#x_pt_dada[3,12] = 2
 
 
 # Variable de estado Y-p,t,s,m
@@ -235,225 +237,225 @@ for p in P:
 
 # Valores Iniciales
 
-y_pstm_dada[1,1,1,1] = 1
-
-y_pstm_dada[1,1,1,3] = 1
-
-y_pstm_dada[1,1,1,5] = 1
-
-y_pstm_dada[2,1,1,7] = 1
-
-y_pstm_dada[3,1,1,29] = 1
-
-u_pstm_dada[1,1,1,2] = 1
-
-u_pstm_dada[1,1,1,4] = 1
-
-u_pstm_dada[1,1,1,6] = 1
-
-u_pstm_dada[2,1,1,28] = 1
-
-u_pstm_dada[3,1,1,52] = 1
-
-y_pstm_dada[1,2,2,1] = 1
-
-y_pstm_dada[1,2,2,3] = 1
-
-y_pstm_dada[1,2,2,5] = 1
-
-y_pstm_dada[2,2,2,7] = 1
-
-y_pstm_dada[2,2,2,29] = 1
-
-u_pstm_dada[1,2,2,2] = 1
-
-u_pstm_dada[1,2,2,4] = 1
-
-u_pstm_dada[1,2,2,6] = 1
-
-u_pstm_dada[2,2,2,28] = 1
-
-u_pstm_dada[2,2,2,50] = 1
-
-y_pstm_dada[1,3,3,1] = 1
-
-y_pstm_dada[1,3,3,3] = 1
-
-y_pstm_dada[1,3,3,5] = 1
-
-y_pstm_dada[1,1,3,45] = 1
-
-y_pstm_dada[2,2,3,23] = 1
-
-y_pstm_dada[3,2,3,7] = 1
-
-y_pstm_dada[3,2,3,15] = 1
-
-u_pstm_dada[1,3,3,2] = 1
-
-u_pstm_dada[1,3,3,4] = 1
-
-u_pstm_dada[1,3,3,6] = 1
-
-u_pstm_dada[1,1,3,46] = 1
-
-u_pstm_dada[2,2,3,44] = 1
-
-u_pstm_dada[3,2,3,14] = 1
-
-u_pstm_dada[3,2,3,22] = 1
-
-y_pstm_dada[1,2,4,1] = 1
-
-u_pstm_dada[1,2,4,2] = 1
-
-y_pstm_dada[1,1,5,27] = 1
-
-y_pstm_dada[1,3,5,25] = 1
-
-y_pstm_dada[2,1,5,29] = 1
-
-y_pstm_dada[3,3,5,1] = 1
-
-y_pstm_dada[3,3,5,13] = 1
-
-u_pstm_dada[1,1,5,28] = 1
-
-u_pstm_dada[1,3,5,26] = 1
-
-u_pstm_dada[2,1,5,50] = 1
-
-u_pstm_dada[3,3,5,12] = 1
-
-u_pstm_dada[3,3,5,14] = 1
-
-y_pstm_dada[1,2,6,13] = 1
-
-y_pstm_dada[1,4,6,1] = 1
-
-y_pstm_dada[1,4,6,5] = 1
-
-y_pstm_dada[1,4,6,9] = 1
-
-y_pstm_dada[2,2,6,15] = 1
-
-u_pstm_dada[1,2,6,14] = 1
-
-u_pstm_dada[1,4,6,4] = 1
-
-u_pstm_dada[1,4,6,8] = 1
-
-u_pstm_dada[1,4,6,12] = 1
-
-u_pstm_dada[2,2,6,46] = 1
-
-y_pstm_dada[1,5,7,1] = 1
-
-y_pstm_dada[1,5,7,5] = 1
-
-y_pstm_dada[1,5,7,9] = 1
-
-y_pstm_dada[1,3,7,29] = 1
-
-y_pstm_dada[2,1,7,31] = 1
-
-y_pstm_dada[3,4,7,13] = 1
-
-y_pstm_dada[3,4,7,21] = 1
-
-u_pstm_dada[1,5,7,4] = 1
-
-u_pstm_dada[1,5,7,8] = 1
-
-u_pstm_dada[1,5,7,12] = 1
-
-u_pstm_dada[1,3,7,30] = 1
-
-u_pstm_dada[2,1,7,52] = 1
-
-u_pstm_dada[3,4,7,20] = 1
-
-u_pstm_dada[3,4,7,28] = 1
-
-y_pstm_dada[1,4,8,13] = 1
-
-y_pstm_dada[1,6,8,1] = 1
-
-y_pstm_dada[1,6,8,5] = 1
-
-y_pstm_dada[1,6,8,9] = 1
-
-y_pstm_dada[2,2,8,17] = 1
-
-u_pstm_dada[1,4,8,16] = 1
-
-u_pstm_dada[1,6,8,4] = 1
-
-u_pstm_dada[1,6,8,8] = 1
-
-u_pstm_dada[1,6,8,12] = 1
-
-u_pstm_dada[2,2,8,38] = 1
-
-y_pstm_dada[1,5,9,47] = 1
-
-y_pstm_dada[2,3,9,1] = 1
-
-y_pstm_dada[3,5,9,23] = 1
-
-y_pstm_dada[3,5,9,31] = 1
-
-u_pstm_dada[1,5,9,50] = 1
-
-u_pstm_dada[2,3,9,22] = 1
-
-u_pstm_dada[3,5,9,30] = 1
-
-u_pstm_dada[3,5,9,38] = 1
-
-y_pstm_dada[1,4,10,49] = 1
-
-y_pstm_dada[1,6,10,45] = 1
-
-y_pstm_dada[2,4,10,1] = 1
-
-y_pstm_dada[2,3,10,23] = 1
-
-u_pstm_dada[1,4,10,52] = 1
-
-u_pstm_dada[1,6,10,48] = 1
-
-u_pstm_dada[2,4,10,22] = 1
-
-u_pstm_dada[2,3,10,44] = 1
-
-y_pstm_dada[1,5,11,39] = 1
-
-y_pstm_dada[2,4,11,17] = 1
-
-y_pstm_dada[3,6,11,1] = 1
-
-y_pstm_dada[3,6,11,9] = 1
-
-u_pstm_dada[1,5,11,42] = 1
-
-u_pstm_dada[2,4,11,38] = 1
-
-u_pstm_dada[3,6,11,8] = 1
-
-u_pstm_dada[3,6,11,16] = 1
-
-y_pstm_dada[1,6,12,1] = 1
-
-y_pstm_dada[3,1,12,5] = 1
-
-y_pstm_dada[3,1,12,17] = 1
-
-u_pstm_dada[1,6,12,4] = 1
-
-u_pstm_dada[3,1,12,16] = 1
-
-u_pstm_dada[3,1,12,28] = 1
+#y_pstm_dada[1,1,1,1] = 1
+#
+#y_pstm_dada[1,1,1,3] = 1
+#
+#y_pstm_dada[1,1,1,5] = 1
+#
+#y_pstm_dada[2,1,1,7] = 1
+#
+#y_pstm_dada[3,1,1,29] = 1
+#
+#u_pstm_dada[1,1,1,2] = 1
+#
+#u_pstm_dada[1,1,1,4] = 1
+#
+#u_pstm_dada[1,1,1,6] = 1
+#
+#u_pstm_dada[2,1,1,28] = 1
+#
+#u_pstm_dada[3,1,1,52] = 1
+#
+#y_pstm_dada[1,2,2,1] = 1
+#
+#y_pstm_dada[1,2,2,3] = 1
+#
+#y_pstm_dada[1,2,2,5] = 1
+#
+#y_pstm_dada[2,2,2,7] = 1
+#
+#y_pstm_dada[2,2,2,29] = 1
+#
+#u_pstm_dada[1,2,2,2] = 1
+#
+#u_pstm_dada[1,2,2,4] = 1
+#
+#u_pstm_dada[1,2,2,6] = 1
+#
+#u_pstm_dada[2,2,2,28] = 1
+#
+#u_pstm_dada[2,2,2,50] = 1
+#
+#y_pstm_dada[1,3,3,1] = 1
+#
+#y_pstm_dada[1,3,3,3] = 1
+#
+#y_pstm_dada[1,3,3,5] = 1
+#
+#y_pstm_dada[1,1,3,45] = 1
+#
+#y_pstm_dada[2,2,3,23] = 1
+#
+#y_pstm_dada[3,2,3,7] = 1
+#
+#y_pstm_dada[3,2,3,15] = 1
+#
+#u_pstm_dada[1,3,3,2] = 1
+#
+#u_pstm_dada[1,3,3,4] = 1
+#
+#u_pstm_dada[1,3,3,6] = 1
+#
+#u_pstm_dada[1,1,3,46] = 1
+#
+#u_pstm_dada[2,2,3,44] = 1
+#
+#u_pstm_dada[3,2,3,14] = 1
+#
+#u_pstm_dada[3,2,3,22] = 1
+#
+#y_pstm_dada[1,2,4,1] = 1
+#
+#u_pstm_dada[1,2,4,2] = 1
+#
+#y_pstm_dada[1,1,5,27] = 1
+#
+#y_pstm_dada[1,3,5,25] = 1
+#
+#y_pstm_dada[2,1,5,29] = 1
+#
+#y_pstm_dada[3,3,5,1] = 1
+#
+#y_pstm_dada[3,3,5,13] = 1
+#
+#u_pstm_dada[1,1,5,28] = 1
+#
+#u_pstm_dada[1,3,5,26] = 1
+#
+#u_pstm_dada[2,1,5,50] = 1
+#
+#u_pstm_dada[3,3,5,12] = 1
+#
+#u_pstm_dada[3,3,5,14] = 1
+#
+#y_pstm_dada[1,2,6,13] = 1
+#
+#y_pstm_dada[1,4,6,1] = 1
+#
+#y_pstm_dada[1,4,6,5] = 1
+#
+#y_pstm_dada[1,4,6,9] = 1
+#
+#y_pstm_dada[2,2,6,15] = 1
+#
+#u_pstm_dada[1,2,6,14] = 1
+#
+#u_pstm_dada[1,4,6,4] = 1
+#
+#u_pstm_dada[1,4,6,8] = 1
+#
+#u_pstm_dada[1,4,6,12] = 1
+#
+#u_pstm_dada[2,2,6,46] = 1
+#
+#y_pstm_dada[1,5,7,1] = 1
+#
+#y_pstm_dada[1,5,7,5] = 1
+#
+#y_pstm_dada[1,5,7,9] = 1
+#
+#y_pstm_dada[1,3,7,29] = 1
+#
+#y_pstm_dada[2,1,7,31] = 1
+#
+#y_pstm_dada[3,4,7,13] = 1
+#
+#y_pstm_dada[3,4,7,21] = 1
+#
+#u_pstm_dada[1,5,7,4] = 1
+#
+#u_pstm_dada[1,5,7,8] = 1
+#
+#u_pstm_dada[1,5,7,12] = 1
+#
+#u_pstm_dada[1,3,7,30] = 1
+#
+#u_pstm_dada[2,1,7,52] = 1
+#
+#u_pstm_dada[3,4,7,20] = 1
+#
+#u_pstm_dada[3,4,7,28] = 1
+#
+#y_pstm_dada[1,4,8,13] = 1
+#
+#y_pstm_dada[1,6,8,1] = 1
+#
+#y_pstm_dada[1,6,8,5] = 1
+#
+#y_pstm_dada[1,6,8,9] = 1
+#
+#y_pstm_dada[2,2,8,17] = 1
+#
+#u_pstm_dada[1,4,8,16] = 1
+#
+#u_pstm_dada[1,6,8,4] = 1
+#
+#u_pstm_dada[1,6,8,8] = 1
+#
+#u_pstm_dada[1,6,8,12] = 1
+#
+#u_pstm_dada[2,2,8,38] = 1
+#
+#y_pstm_dada[1,5,9,47] = 1
+#
+#y_pstm_dada[2,3,9,1] = 1
+#
+#y_pstm_dada[3,5,9,23] = 1
+#
+#y_pstm_dada[3,5,9,31] = 1
+#
+#u_pstm_dada[1,5,9,50] = 1
+#
+#u_pstm_dada[2,3,9,22] = 1
+#
+#u_pstm_dada[3,5,9,30] = 1
+#
+#u_pstm_dada[3,5,9,38] = 1
+#
+#y_pstm_dada[1,4,10,49] = 1
+#
+#y_pstm_dada[1,6,10,45] = 1
+#
+#y_pstm_dada[2,4,10,1] = 1
+#
+#y_pstm_dada[2,3,10,23] = 1
+#
+#u_pstm_dada[1,4,10,52] = 1
+#
+#u_pstm_dada[1,6,10,48] = 1
+#
+#u_pstm_dada[2,4,10,22] = 1
+#
+#u_pstm_dada[2,3,10,44] = 1
+#
+#y_pstm_dada[1,5,11,39] = 1
+#
+#y_pstm_dada[2,4,11,17] = 1
+#
+#y_pstm_dada[3,6,11,1] = 1
+#
+#y_pstm_dada[3,6,11,9] = 1
+#
+#u_pstm_dada[1,5,11,42] = 1
+#
+#u_pstm_dada[2,4,11,38] = 1
+#
+#u_pstm_dada[3,6,11,8] = 1
+#
+#u_pstm_dada[3,6,11,16] = 1
+#
+#y_pstm_dada[1,6,12,1] = 1
+#
+#y_pstm_dada[3,1,12,5] = 1
+#
+#y_pstm_dada[3,1,12,17] = 1
+#
+#u_pstm_dada[1,6,12,4] = 1
+#
+#u_pstm_dada[3,1,12,16] = 1
+#
+#u_pstm_dada[3,1,12,28] = 1
 
 
 ###########
@@ -483,12 +485,12 @@ for c in C:
 
     if c == contador_de_columnas:
 
-        k_c[c] = quicksum(z_p_dada[p]*CD[p-1] for p in P) + \
-                    quicksum(hora_extra * u_pstm_dada[p,s,t,m] * CM  
-                        for hora_extra in BE2 for p in P for s in S[p] for t in T for m in range(BR2 + hora_extra, 53))
+        k_c[c] = quicksum(z_p_dada[p]*CD[p] for p in P) + quicksum(hora_extra * u_pstm_dada[p,s,t,m] * CM for hora_extra in BE2 for p in P for s in S[p] for t in T for m in range(BR2 + hora_extra, 41))
 
 # ¿Porque p-1 en cdp? Para alinear protocolos (1,2,3) con índice de búsqueda (0,1,2)
-
+#pero es un diccionario
+#el hora extra estaría multiplicando como 1*u+2*u+3*u???
+#aparecía hasta 51, deberia ser 43 o no?
 # OMEGA
 
 omega_cpst = {}
@@ -503,9 +505,9 @@ for c in C:
 
                 for t in T:
 
-                    if (t + 6 - K_ps[p][s]) > 0 and (t + 6) < 13:
+                    if (int(t) + 6 - K_ps[p][s-1]) > 0 and (int(t) + 6) < 13:
 
-                        omega_cpst[c,p,s,t] = w_pst_dada[p,s,t] - γ * (w_pst_dada[p,s,(t + 6)] + x_pt_dada[p,(t + 6 - K_ps[p][s])])
+                        omega_cpst[c,p,s,t] = w_pst_dada[p,s,t] - γ * (w_pst_dada[p,s,(t + 6)] + x_pt_dada[p,(t + 6 - K_ps[p][s-1])])
 
                     else:
 
